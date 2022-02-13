763ccf33d23a01e1663a6757a811a1a33d59f96f
jahess-it <jahess.it@gmail.com>
2022-02-12 20:25:08 -0700

    Add calendar views/functions

---
+++ functions.sql
@@ -0,0 +1,25 @@
+CREATE FUNCTION get_week_start() RETURNS DATE AS
+    $$ SELECT CURRENT_DATE - EXTRACT(DOW FROM CURRENT_DATE)::INT4 $$
+    LANGUAGE SQL;
+
+CREATE FUNCTION get_month_start() RETURNS DATE AS
+    $$ SELECT CURRENT_DATE - EXTRACT(DAY FROM CURRENT_DATE)::INT4 + 1 $$
+    LANGUAGE SQL;
+
+CREATE FUNCTION get_calendar(start DATE, finish DATE) RETURNS TABLE(
+    habit_id INT4,
+    title VARCHAR(255),
+    day DATE,
+    complete BOOL,
+    time_spent INTERVAL HOUR TO SECOND (0),
+    habit_rating FLOAT4,
+    display_color CHAR(7))
+    AS $$
+    SELECT h.habit_id, h.title, i.day, i.complete, i.time_spent, i.habit_rating, c.display_color
+    FROM habit h
+    INNER JOIN habit_instance i ON h.habit_id = i.habit_id
+    INNER JOIN category c ON h.category_name = c.category_name
+    WHERE day BETWEEN start AND finish
+    $$
+    LANGUAGE SQL;
+
--- views.sql
+++ views.sql
@@ -1,8 +1,16 @@
+CREATE VIEW week AS
+    SELECT * FROM get_calendar(get_week_start(),
+        get_week_start() + 6);
+
+CREATE VIEW month AS
+    SELECT * FROM get_calendar(get_month_start(),
+        (get_month_start() + INTERVAL '1 month -1 day')::DATE);
+
 CREATE VIEW journals AS
     SELECT day, journal, day_rating
-    FROM day_summary
-    WHERE username = 'jonsnow';
+    FROM day_summary;
 
 CREATE VIEW admin AS
     SELECT username, title
-    FROM habit
+    FROM habit;
+
