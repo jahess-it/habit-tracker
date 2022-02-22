8cf7d1ce14c1c04ed026275f317902735b1fed29
jahess-it <jahess.it@gmail.com>
2022-02-22 14:20:38 -0700

    Moved views into API schema

--- views.sql
+++ views.sql
@@ -1,20 +1,22 @@
-CREATE VIEW day AS
+CREATE SCHEMA api;
+
+CREATE VIEW api.upcoming AS
     SELECT * FROM get_calendar(CURRENT_DATE),
         CURRENT_DATE + 1);
 
-CREATE VIEW week AS
+CREATE VIEW api.week AS
     SELECT * FROM get_calendar(get_week_start(),
         get_week_start() + 6);
 
-CREATE VIEW month AS
+CREATE VIEW api.month AS
     SELECT * FROM get_calendar(get_month_start(),
         (get_month_start() + INTERVAL '1 month -1 day')::DATE);
 
-CREATE VIEW journals AS
+CREATE VIEW api.journals AS
     SELECT day, journal, day_rating
     FROM day_summary;
 
-CREATE VIEW admin AS
+CREATE VIEW api.admin AS
     SELECT username, title
     FROM habit;
 
