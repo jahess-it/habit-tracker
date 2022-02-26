18f3dc16bf0a54888ae0812ddb44a7b8a5e33764
jahess-it <jahess.it@gmail.com>
2022-02-26 14:19:40 -0700

    Moved views to public schema and added replace syntax

--- views.sql
+++ views.sql
@@ -1,22 +1,20 @@
-CREATE SCHEMA api;
-
-CREATE VIEW api.upcoming AS
+CREATE OR REPLACE VIEW upcoming AS
     SELECT * FROM get_calendar(CURRENT_DATE,
         CURRENT_DATE + 1);
 
-CREATE VIEW api.week AS
+CREATE OR REPLACE VIEW week AS
     SELECT * FROM get_calendar(get_week_start(),
         get_week_start() + 6);
 
-CREATE VIEW api.month AS
+CREATE OR REPLACE VIEW month AS
     SELECT * FROM get_calendar(get_month_start(),
         (get_month_start() + INTERVAL '1 month -1 day')::DATE);
 
-CREATE VIEW api.journals AS
+CREATE OR REPLACE VIEW journals AS
     SELECT day, journal, day_rating
     FROM day_summary;
 
-CREATE VIEW api.admin AS
+CREATE OR REPLACE VIEW admin AS
     SELECT username, title
     FROM habit;
 
