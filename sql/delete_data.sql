4c2894d7bb93374c7048614cf74357ef1a759773
jahess-it <jahess.it@gmail.com>
2022-03-03 16:45:24 -0700

    Imported sample project

---
+++ sql/delete_data.sql
@@ -0,0 +1,9 @@
+DELETE FROM habit_instance;
+
+DELETE FROM habit;
+
+DELETE FROM day_summary;
+
+DELETE FROM category;
+
+DELETE FROM user_account;
---
+++ sql/views.sql
@@ -0,0 +1,20 @@
+CREATE OR REPLACE VIEW upcoming AS
+    SELECT * FROM get_calendar(CURRENT_DATE,
+        CURRENT_DATE + 1);
+
+CREATE OR REPLACE VIEW week AS
+    SELECT * FROM get_calendar(get_week_start(),
+        get_week_start() + 6);
+
+CREATE OR REPLACE VIEW month AS
+    SELECT * FROM get_calendar(get_month_start(),
+        (get_month_start() + INTERVAL '1 month -1 day')::DATE);
+
+CREATE OR REPLACE VIEW journal AS
+    SELECT day, username, journal, day_rating
+    FROM day_summary;
+
+CREATE OR REPLACE VIEW admin AS
+    SELECT username, title
+    FROM habit;
+
