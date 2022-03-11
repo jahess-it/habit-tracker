8a379f9d534dc27711042aaacb878a6a9b5b1315
Rhemsley <77403987+Rhemsley@users.noreply.github.com>
2022-03-11 03:59:01 -0700

    Nope cant remove imports

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -17,6 +17,8 @@
 import Api from "../api";
 import { } from '../auth';
 import Habit from "../components/Habit.vue";
+import { getJwtToken } from "../auth";
+import { getUserIdFromToken } from "../auth";
 
 export default {
   name: "Home",
