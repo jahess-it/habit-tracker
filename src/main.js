472e4962cd57c126fc3de97475f06d52de415cce
Rhemsley <77403987+Rhemsley@users.noreply.github.com>
2022-03-11 03:27:22 -0700

    add devtool while developing

--- src/main.js
+++ src/main.js
@@ -14,6 +14,7 @@ import AdminArticleList from "./components/AdminArticleList";
 import AdminArticleEdit from "./components/AdminArticleEdit";
 
 Vue.config.productionTip = false;
+Vue.config.devtools = true;
 
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
