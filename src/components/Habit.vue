bdc973b9fa34eb6dff8be2f249f0d698c4868dc9
Rhemsley <77403987+Rhemsley@users.noreply.github.com>
2022-03-11 04:25:07 -0700

    giving up on html for now

--- src/components/Habit.vue
+++ src/components/Habit.vue
@@ -1,12 +1,5 @@
 <template>
   <div :style="{'background-color': this.habit.display_color}">
-    <p>
-    <span :style="float:left"> {{this.habit.title}} </span>
-    <span> {{this.habit.description}} </span>
-    <span :style="float:right"> {{this.habit.cetegory_name}} </span>
-    </p>
-  </div>
-  <div :style="{'background-color': this.habit.display_color}">
     <p>{{this.habit.title}}</p>
   </div>
 </template>
