d303ad70a07c930b10aa2b23a63a4a76a1f46932
Rhemsley <77403987+Rhemsley@users.noreply.github.com>
2022-03-12 07:06:42 -0700

    Add navbar script

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,15 @@ export default {
   name: "App",
   components: {},
 };
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
 </script>
 
 <style>
