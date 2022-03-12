df24b134afecd17df5ead43e5237cb69a46075c5
Rhemsley <77403987+Rhemsley@users.noreply.github.com>
2022-03-12 07:14:46 -0700

    Move Navbar test

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,15 +1,15 @@
 <template>
-<div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="#news">News</a>
-  <a href="#contact">Contact</a>
-  <a href="#about">About</a>
-</div>
   <div>
     <b-jumbotron>
       <h1 style="text-align: center">Habit Tracker</h1>
     </b-jumbotron>
     <br />
+    <div class="topnav">
+      <a class="active" href="#home">Home</a>
+      <a href="#news">News</a>
+      <a href="#contact">Contact</a>
+      <a href="#about">About</a>
+    </div>
     <div v-if="loading">Loading habits . . .</div>
     <ul v-else>
       <div v-for="habit in habits" :key="habit.habit_id">
