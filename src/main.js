b3ec921d3fc025453b244aa70d1ffdc8178413f0
Rhemsley <77403987+Rhemsley@users.noreply.github.com>
2022-03-12 08:45:18 -0700

    Hide Month and week for now

--- src/main.js
+++ src/main.js
@@ -8,8 +8,8 @@ import Admin from "./pages/Admin";
 import Login from "./pages/Login";
 import Logout from "./pages/Logout";
 import Register from "./pages/Register";
-import Week from "./pages/Week";
-import Month from "./pages/Month";
+//import Week from "./pages/Week";
+//import Month from "./pages/Month";
 import AdminArticleAdd from "./components/AdminArticleAdd";
 import AdminArticleList from "./components/AdminArticleList";
 import AdminArticleEdit from "./components/AdminArticleEdit";
@@ -42,8 +42,8 @@ const router = new VueRouter({
   mode: "history",
   routes: [
     { path: "/", component: Home, beforeEnter: checkAuth },
-    { path: "/week", component: Week, beforeEnter: checkAuth },
-    { path: "/month", component: Month, beforeEnter: checkAuth },
+    { //path: "/week", component: Week, beforeEnter: checkAuth },
+    { //path: "/month", component: Month, beforeEnter: checkAuth },
     { path: "/login", component: Login },
     { path: "/logout", component: Logout },
     { path: "/register", component: Register },
