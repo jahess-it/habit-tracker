d5350034e2cad7eddffee44a696c89bd04a11861
Rhemsley <77403987+Rhemsley@users.noreply.github.com>
2022-03-14 20:36:09 -0600

    add a habits file for showing the users habits

---
+++ src/components/Habits.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <p>{{this.habit.title}}
+    Description: {{this.habit.description}}
+    Timed: {{this.habit.timed}}
+    Ratable: {{this.habit.ratable}}
+    category_name: {{this.habit.category_name}}
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "habit",
+    props: {
+      habit: Object
+    },
+    data: function () {
+      return {};
+    },
+    created: function () {}
+  };
+</script>
