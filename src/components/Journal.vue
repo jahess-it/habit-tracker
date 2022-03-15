7452d6f82cbc50b7df837b0e0c4f085b48eaffc0
Rhemsley <77403987+Rhemsley@users.noreply.github.com>
2022-03-15 10:07:46 -0600

    Add journal component

---
+++ src/components/Journal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div style="background-color: #ADD8E6">
+    <p>
+    <b>Day: {{this.habit.day}}</b>
+    Entry: {{this.habit.journal}}
+    Rating: {{this.habit.day_rating}}
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "journal",
+    props: {
+      journal: Object
+    },
+    data: function () {
+      return {};
+    },
+    created: function () {}
+  };
+</script>
