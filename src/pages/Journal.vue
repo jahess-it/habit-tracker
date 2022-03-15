2c49ad00b57af3e3891141a4a5481d34c38cea04
Rhemsley <77403987+Rhemsley@users.noreply.github.com>
2022-03-15 10:12:39 -0600

    Add Journal Page

---
+++ src/pages/Journal.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div class="topnav">
+      <a href="/">Home</a>
+      <a href="/Week">Week</a>
+      <a href="/Month">Month</a>
+      <a class="active" href="/">Journal</a>
+    </div>
+    <br />
+    <div>
+      <h1 style="text-align: center">Habit Tracker</h1>
+    </div>
+    <div>
+      <h2 style="text-align: center">Your Journal</h2>
+    </div>
+    <div v-if="loading">Loading journals . . .</div>
+    <ul v-else>
+      <div v-for="journal in journals" :key="journal.day">
+        <Journal :journal="journal"></Journal>
+      </div>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Api from "../api";
+import { } from '../auth';
+import Journal from "../components/Journal.vue";
+import { getJwtToken } from "../auth";
+import { getUserIdFromToken } from "../auth";
+export default {
+  name: "Journal",
+  data: function () {
+    return {
+      loading: false,
+      journals: []
+    };
+  },
+  created: function () {
+    this.loading = true;
+    this.token = getJwtToken();
+    this.username = getUserIdFromToken(this.token);
+    Api.getJournal(this.username).then((res) => {
+      this.journals = res.data;
+      this.loading = false;
+    });
+  },
+  components: { Journal }
+};
+</script>
