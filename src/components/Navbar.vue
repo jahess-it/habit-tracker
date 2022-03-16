05eafba12379049e75125a2fc8f0237dd2e90582
James Hess <jahess.it@gmail.com>
2022-03-16 15:05:25 -0600

    Edit link to Create Habit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -4,7 +4,7 @@
     <a :class="{ active: location == 'week' }" href="/week">Week</a>
     <a :class="{ active: location == 'month' }" href="/month">Month</a>
     <a :class="{ active: location == 'journals' }" href="/journals">Journals</a>
-    <a :class="{ active: location == 'createhabit' }" href="/create_habit">Create Habit</a>
+    <a :class="{ active: location == 'createhabit' }" href="/createhabit">Create Habit</a>
     <a :class="{ active: location == 'admin' }" style="float:right" href="/admin">Admin</a>
     <a :class="{ active: location == 'logout' }" style="float:right" href="/logout">Logout</a>
     <a :class="{ active: location == 'account' }" style="float:right" href="/account">Account</a>
