de962f147e6f95837af2a8de349a361e0d006f6f
Rhemsley <77403987+Rhemsley@users.noreply.github.com>
2022-03-16 13:27:44 -0600

    Change habit to account

--- src/pages/Account.vue
+++ src/pages/Account.vue
@@ -23,7 +23,7 @@
       <p>
       Email: {{this.account.email}} <button type="button">Change Email</button>
       </p>
-      Mobile Phone: {{this.habit.mobile_phone}} <button type="button">Change Number</button>
+      Mobile Phone: {{this.account.mobile_phone}} <button type="button">Change Number</button>
       <p>
       <button type="button">Change Password</button>
       </p>
