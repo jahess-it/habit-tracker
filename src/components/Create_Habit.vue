0b3b281a0eafb132bc328c734ddcd22a7e3b44c3
Kaden Hardy <67716541+kadenhardy@users.noreply.github.com>
2022-03-16 14:08:54 -0600

    Update Create_Habit.vue

--- src/components/Create_Habit.vue
+++ src/components/Create_Habit.vue
@@ -1,1 +1,77 @@
+<template>
+  <div>
+    <b-breadcrumb>
+      <b-breadcrumb-item active>Create Habit</b-breadcrumb-item>
+    </b-breadcrumb>
+    <div class="card card-container p-4">
+      <form name="form" @submit.prevent="handleAdd">
+        <div>
+          <div class="form-group">
+            <label for="username">Title</label>
+            <input
+              v-model="title"
+              type="text"
+              class="form-control"
+              name="title"
+            />
+          </div>
+          <div class="form-group">
+            <label for="content">content</label>
+            <textarea
+              v-model="content"
+              class="form-control"
+              name="content"
+              rows="20"
+            />
+          </div>
+          <div class="form-group">
+            <button class="btn btn-primary btn-block" :disabled="loading">
+              <span
+                v-show="loading"
+                class="spinner-border spinner-border-sm"
+              ></span>
+              <span>Add Habit</span>
+            </button>
+          </div>
+        </div>
+      </form>
 
+      <div v-if="message" class="alert alert-danger">
+        {{ message }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Api from "../api";
+export default {
+  name: "Create_Habit",
+  data() {
+    return {
+      title: "",
+      content: "",
+      loading: false,
+      message: "",
+    };
+  },
+  methods: {
+    handleAdd() {
+      this.loading = true;
+      this.message = "";
+      Api.addHabit({ title: this.title, content: this.content })
+        .then(() => {
+          this.loading = false;
+          this.$router.push("/admin/");
+        })
+        .catch((error) => {
+          console.log(error);
+          if (error.response) {
+            this.message = error.response.data.message;
+          }
+          this.loading = false;
+        });
+    },
+  },
+};
+</script>
