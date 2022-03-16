50a3be1f9ec86157cd0e90537791fbbe38cbbaf0
James Hess <jahess.it@gmail.com>
2022-03-16 14:35:15 -0600

    Create navbar.vue

---
+++ src/components/navbar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="topnav">
+    <a :class="{ active: location == 'home' }" href="/">Home</a>
+    <a :class="{ active: location == 'week' }" href="/week">Week</a>
+    <a :class="{ active: location == 'month' }" href="/month">Month</a>
+    <a :class="{ active: location == 'journals' }" href="/journals">Journals</a>
+    <a :class="{ active: location == 'create_habits' }" href="/create_habit">Create Habit</a>
+    <a :class="{ active: location == 'logout' }" style="float:right" href="/logout">Logout</a>
+    <a :class="{ active: location == 'account' }" style="float:right" href="/account">Account</a>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "navbar",
+    props: {
+      location: String
+    },
+    data: function () {
+      return {};
+    },
+    created: function () {}
+  };
+</script>
