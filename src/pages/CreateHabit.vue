4cdc545a6d74908d1b5c50de28cd872365eb6eda
James Hess <jahess.it@gmail.com>
2022-03-16 15:07:20 -0600

    Use new navbar

--- src/pages/CreateHabit.vue
+++ src/pages/CreateHabit.vue
@@ -1,14 +1,6 @@
 <template>
   <div>
-    <div class="topnav">
-      <a href="/">Home</a>
-      <a href="/Week">Week</a>
-      <a href="/Month">Month</a>
-      <a href="/Journals">Journals</a>      
-      <a class="active" href="/CreateHabit">Create Habit</a>
-      <a style="float:right" href="/logout">Logout</a>
-      <a style="float:right" href="/Account">Account</a>
-    </div>
+    <Navbar location="createhabit"></Navbar>
   </div>
 </template>
 
@@ -17,9 +9,11 @@ import Api from "../api";
 import { } from '../auth';
 import HabitCreator from "../components/HabitCreator.vue";
 import Habit from "../components/Habit.vue";
+import Navbar from "../components/Navbar.vue";
 import { getJwtToken } from "../auth";
 import { getUserIdFromToken } from "../auth";
+
 export default {
-  
+  components: { HabitCreator, Habit, Navbar }
 };
 </script>
