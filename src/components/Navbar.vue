aa7937e6cd10b1698f562bfe4418943b5e287c96
James Hess <jahess.it@gmail.com>
2022-03-16 15:14:40 -0600

    Move admin button

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,9 +5,9 @@
     <a :class="{ active: location == 'month' }" href="/month">Month</a>
     <a :class="{ active: location == 'journals' }" href="/journals">Journals</a>
     <a :class="{ active: location == 'createhabit' }" href="/createhabit">Create Habit</a>
-    <a :class="{ active: location == 'admin' }" style="float:right" href="/admin">Admin</a>
     <a :class="{ active: location == 'logout' }" style="float:right" href="/logout">Logout</a>
     <a :class="{ active: location == 'account' }" style="float:right" href="/account">Account</a>
+    <a :class="{ active: location == 'admin' }" style="float:right" href="/admin">Admin</a>
   </div>
 </template>
 
