1c8f6b0810819aa34d1108efb6f2a45d171cf3e7
Kaden Hardy <67716541+kadenhardy@users.noreply.github.com>
2022-03-16 14:27:32 -0600

    Update CreateHabit.vue

--- src/pages/CreateHabit.vue
+++ src/pages/CreateHabit.vue
@@ -1,1 +1,52 @@
+<template>
+  <div>
+    <div class="topnav">
+      <a href="/">Home</a>
+      <a class="active" href="/Week">Week</a>
+      <a href="/Month">Month</a>
+      <a href="/Journals">Journals</a>
+      <a style="float:right" href="/logout">Logout</a>
+      <a style="float:right" href="/Account">Account</a>
+    </div>
+    <br />
+    <div>
+      <h1 style="text-align: center">Habit Tracker</h1>
+    </div>
+    <div>
+      <h2 style="text-align: center">This Week</h2>
+    </div>
+    <div v-if="loading">Loading habits . . .</div>
+    <div v-else>
+      <div v-for="habit in habits" :key="habit.habit_id">
+        <Habit :habit="habit"></Habit>
+      </div>
+    </div>
+  </div>
+</template>
 
+<script>
+import Api from "../api";
+import { } from '../auth';
+import Habit from "../components/Habit.vue";
+import { getJwtToken } from "../auth";
+import { getUserIdFromToken } from "../auth";
+export default {
+  name: "Week",
+  data: function () {
+    return {
+      loading: false,
+      habits: []
+    };
+  },
+  created: function () {
+    this.loading = true;
+    this.token = getJwtToken();
+    this.username = getUserIdFromToken(this.token);
+    Api.getWeekView(this.username).then((res) => {
+      this.habits = res.data;
+      this.loading = false;
+    });
+  },
+  components: { Habit }
+};
+</script>
