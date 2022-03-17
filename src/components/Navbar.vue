16fe84c3ca60f65b4e63b60c9b5ea2717d3f4f49
Kaden Hardy <67716541+kadenhardy@users.noreply.github.com>
2022-03-17 17:18:10 -0600

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,6 +5,7 @@
     <a :class="{ active: location == 'month' }" href="/month">Month</a>
     <a :class="{ active: location == 'journals' }" href="/journals">Journals</a>
     <a :class="{ active: location == 'createhabit' }" href="/createhabit">Create Habit</a>
+    <a :class="{ active: location == 'createcategory' }" href="/createcategory">Create Category</a>
     <a :class="{ active: location == 'logout' }" style="float:right" href="/logout">Logout</a>
     <a :class="{ active: location == 'account' }" style="float:right" href="/account">Account</a>
     <a :class="{ active: location == 'admin' }" style="float:right" href="/admin">Admin</a>
