175efd1c39535cba4d90f82908c17ed427c8a944
Kaden Hardy <me@kadenhardy.com>
2022-03-17 10:40:21 -0600

    update create habit form

--- src/components/HabitCreator.vue
+++ src/components/HabitCreator.vue
@@ -56,57 +56,58 @@
           </div>
 
           <div>
-            <label for="habitname">Schedule for these dates:</label> 
-</v-date-picker>
-    </v-col>
-    <v-col
-      cols="12"
-      sm="6"
-    >
-      <v-menu
-        ref="menu"
-        v-model="menu"
-        :close-on-content-click="false"
-        :return-value.sync="dates"
-        transition="scale-transition"
-        offset-y
-        min-width="auto"
-      >
-        <template v-slot:activator="{ on, attrs }">
-          <v-combobox
-            v-model="dates"
-            multiple
-            chips
-            small-chips
-            label="Multiple picker in menu"
-            prepend-icon="mdi-calendar"
-            readonly
-            v-bind="attrs"
-            v-on="on"
-          ></v-combobox>
-        </template>
-        <v-date-picker
-          v-model="dates"
-          multiple
-          no-title
-          scrollable
-        >
-          <v-spacer></v-spacer>
-          <v-btn
-            text
-            color="primary"
-            @click="menu = false"
-          >
-            Cancel
-          </v-btn>
-          <v-btn
-            text
-            color="primary"
-            @click="$refs.menu.save(dates)"
-          >
-            OK
-          </v-btn>
-        </v-date-picker>
+            <label for="habitname">Schedule for these dates:</label>
+            <template>
+              <v-row>
+                <v-col cols="12" sm="6">
+                  <v-date-picker v-model="dates" multiple></v-date-picker>
+                </v-col>
+                <v-col cols="12" sm="6">
+                  <v-menu
+                    ref="menu"
+                    v-model="menu"
+                    :close-on-content-click="false"
+                    :return-value.sync="dates"
+                    transition="scale-transition"
+                    offset-y
+                    min-width="auto"
+                  >
+                    <template v-slot:activator="{ on, attrs }">
+                      <v-combobox
+                        v-model="dates"
+                        multiple
+                        chips
+                        small-chips
+                        label="Multiple picker in menu"
+                        prepend-icon="mdi-calendar"
+                        readonly
+                        v-bind="attrs"
+                        v-on="on"
+                      ></v-combobox>
+                    </template>
+                    <v-date-picker v-model="dates" multiple no-title scrollable>
+                      <v-spacer></v-spacer>
+                      <v-btn text color="primary" @click="menu = false">
+                        Cancel
+                      </v-btn>
+                      <v-btn
+                        text
+                        color="primary"
+                        @click="$refs.menu.save(dates)"
+                      >
+                        OK
+                      </v-btn>
+                    </v-date-picker>
+                  </v-menu>
+                </v-col>
+              </v-row>
+            </template>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
           </div>
 
           <div class="form-group">
