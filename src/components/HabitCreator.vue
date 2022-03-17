9b656004d3d7166be5ceb015d4128ca909a5b1c4
Kaden Hardy <me@kadenhardy.com>
2022-03-17 10:38:07 -0600

    update create habit form

--- src/components/HabitCreator.vue
+++ src/components/HabitCreator.vue
@@ -57,7 +57,56 @@
 
           <div>
             <label for="habitname">Schedule for these dates:</label> 
-            <v-date-picker mode="date" v-model="date" />
+</v-date-picker>
+    </v-col>
+    <v-col
+      cols="12"
+      sm="6"
+    >
+      <v-menu
+        ref="menu"
+        v-model="menu"
+        :close-on-content-click="false"
+        :return-value.sync="dates"
+        transition="scale-transition"
+        offset-y
+        min-width="auto"
+      >
+        <template v-slot:activator="{ on, attrs }">
+          <v-combobox
+            v-model="dates"
+            multiple
+            chips
+            small-chips
+            label="Multiple picker in menu"
+            prepend-icon="mdi-calendar"
+            readonly
+            v-bind="attrs"
+            v-on="on"
+          ></v-combobox>
+        </template>
+        <v-date-picker
+          v-model="dates"
+          multiple
+          no-title
+          scrollable
+        >
+          <v-spacer></v-spacer>
+          <v-btn
+            text
+            color="primary"
+            @click="menu = false"
+          >
+            Cancel
+          </v-btn>
+          <v-btn
+            text
+            color="primary"
+            @click="$refs.menu.save(dates)"
+          >
+            OK
+          </v-btn>
+        </v-date-picker>
           </div>
 
           <div class="form-group">
