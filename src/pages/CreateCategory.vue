bd196ba6bc12556a154ce6a3c8d7c9be8f0299db
Kaden Hardy <67716541+kadenhardy@users.noreply.github.com>
2022-03-17 17:16:25 -0600

    Update CreateCategory.vue

--- src/pages/CreateCategory.vue
+++ src/pages/CreateCategory.vue
@@ -1,1 +1,21 @@
 
+<template>
+  <div>
+    <Navbar location="createcategory"></Navbar>
+    <CategoryCreator></CategoryCreator>
+  </div>
+</template>
+
+<script>
+import Api from "../api";
+import { } from '../auth';
+import CategoryCreator from "../components/CategoryCreator.vue";
+import Habit from "../components/Habit.vue";
+import Navbar from "../components/Navbar.vue";
+import { getJwtToken } from "../auth";
+import { getUserIdFromToken } from "../auth";
+
+export default {
+  components: { CategoryCreator, Navbar }
+};
+</script>
