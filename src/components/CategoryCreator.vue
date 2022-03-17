9c1dc194eb74f9b70a1529a33d82ee8d4e03be2a
Kaden Hardy <me@kadenhardy.com>
2022-03-17 17:22:05 -0600

    update create habit form

--- src/components/CategoryCreator.vue
+++ src/components/CategoryCreator.vue
@@ -4,118 +4,30 @@
       <form name="form" @submit.prevent="handleAdd">
         <div>
           <div class="form-group">
-            <label for="habitname">Habit Name</label>
+            <label for="habitname">Category Name</label>
             <input
-              v-model="charname"
+              v-model="category_name"
               type="text"
               class="form-control"
-              name="title"
+              name="category_name"
             />
           </div>
           <div class="form-group">
-            <label for="habitname">Habit Category</label>
+            <label for="habitname">Display Color</label>
             <input
-              v-model="charname"
+              v-model="display_color"
               type="text"
               class="form-control"
-              name="title"
+              name="display_color"
             />
           </div>
-          <div class="form-group">
-            <label for="habitname">Description (Optional)</label>
-            <input
-              v-model="charname"
-              type="text"
-              class="form-control"
-              name="title"
-            />
-          </div>
-          <div class="form-group">
-            <label class="radio" :for="id">
-              <input
-                type="radio"
-                :id="id"
-                class="radio-button"
-                :value="value"
-                :name="name"
-              />
-              Timed
-            </label>
-          </div>
-          <div class="form-group">
-            <label class="radio" :for="id">
-              <input
-                type="radio"
-                :id="id"
-                class="radio-button"
-                :value="value"
-                :name="name"
-              />
-              Ratable
-            </label>
-          </div>
-
-          <div>
-            <label for="habitname">Schedule for these dates:</label>
-              <v-row>
-                <v-col cols="12" sm="6">
-                  <v-date-picker v-model="dates" multiple></v-date-picker>
-                </v-col>
-                <v-col cols="12" sm="6">
-                  <v-menu
-                    ref="menu"
-                    v-model="menu"
-                    :close-on-content-click="false"
-                    :return-value.sync="dates"
-                    transition="scale-transition"
-                    offset-y
-                    min-width="auto"
-                  >
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-combobox
-                        v-model="dates"
-                        multiple
-                        chips
-                        small-chips
-                        label="Multiple picker in menu"
-                        prepend-icon="mdi-calendar"
-                        readonly
-                        v-bind="attrs"
-                        v-on="on"
-                      ></v-combobox>
-                    </template>
-                    <v-date-picker v-model="dates" multiple no-title scrollable>
-                      <v-spacer></v-spacer>
-                      <v-btn text color="primary" @click="menu = false">
-                        Cancel
-                      </v-btn>
-                      <v-btn
-                        text
-                        color="primary"
-                        @click="$refs.menu.save(dates)"
-                      >
-                        OK
-                      </v-btn>
-                    </v-date-picker>
-                  </v-menu>
-                </v-col>
-              </v-row>
-            </template>
-          </div>
-        </div>
-      </form>
-    </div>
-  </div>
-
-          </div>
-
           <div class="form-group">
             <button class="btn btn-primary btn-block" :disabled="loading">
               <span
                 v-show="loading"
                 class="spinner-border spinner-border-sm"
               ></span>
-              <span>Add Habit</span>
+              <span>Add Category</span>
             </button>
           </div>
         </div>
