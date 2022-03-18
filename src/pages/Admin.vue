5dbd7a21bf6922d6742b6ce5754e3186737c838a
James Hess <jahess.it@gmail.com>
2022-03-17 23:46:28 -0600

    Completely redid admin page

--- src/pages/Admin.vue
+++ src/pages/Admin.vue
@@ -1,25 +1,46 @@
 <template>
+  <Navbar location="admin"></Navbar>
+  
   <div>
-    <b-navbar>
-      <b-navbar-brand :to="{ path: '/' }">Blog Admin</b-navbar-brand>
-
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-
-      <b-collapse id="nav-collapse" is-nav>
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item :to="{ path: '/logout' }">Logout</b-nav-item>
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
-    <br />
-    <hr />
-    <router-view></router-view>
+    <b-form-group>
+      <b-form-radio v-model="show_users" value="true">Show Users</b-form-radio>
+      <b-form-radio v-model="show_users" value="false">Show Habits</b-form-radio>
+    </b-form-group>
+  </div>
+  
+  <div v-if="show_users">
+    <div v-for="user in users" :key="user.username">
+      <p>Placeholder</p>
+    </div>
+  </div>
+  <div v-else>
+    <div v-for="habit in habits" :key="habit.habit_id">
+      <Habit :habit="habit"></Habit>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: "Admin",
-};
+  import Api from "../api";
+  import Navbar from "../components/Navbar.vue";
+  import Habit from "../components/Habit.vue";
+  
+  export default {
+    name: "admin",
+    data: function () {
+      return {
+        show_users: true,
+        users: [],
+        habits: []
+      };
+    },
+    created: function () {
+      this.users = Api.getAllAccounts();
+      this.habits = Api.getAllHabits();
+    },
+    components: {
+      Habit,
+      Navbar
+    }
+  }
 </script>
