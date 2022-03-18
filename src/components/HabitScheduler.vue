6f313533ef2af03bab64ff5e1857369c084a7998
Kaden Hardy <me@kadenhardy.com>
2022-03-17 22:00:15 -0600

    working on habit scheduling

---
+++ src/components/HabitScheduler.vue
@@ -0,0 +1,129 @@
+<template>
+  <div>
+    <div class="card card-container p-4">
+      <form name="form" @submit.prevent="handleAdd">
+        <div>
+          <div class="form-group">
+            <label for="title">Habit Name</label>
+            <input
+              v-model="title"
+              type="text"
+              class="form-control"
+              name="title"
+            />
+          </div>
+          <div class="form-group">
+            <label for="category_name">Habit Category</label>
+            <input
+              v-model="category_name"
+              type="text"
+              class="form-control"
+              name="category_name"
+            />
+          </div>
+          <div class="form-group">
+            <label for="description">Description (Optional)</label>
+            <input
+              v-model="description"
+              type="text"
+              class="form-control"
+              name="description"
+            />
+          </div>
+                    <div class="form-group">
+            <label for="timed">Timed</label>
+            <input
+              v-model="timed"
+              type="text"
+              class="form-control"
+              name="timed"
+            />
+          </div>
+          <div class="form-group">
+            <label for="ratable">Ratable</label>
+            <input
+              v-model="ratable"
+              type="text"
+              class="form-control"
+              name="ratable"
+            />
+          </div>
+          <!-- <div class="form-group">
+            <label class="checkbox" for="timed">
+              <input
+              v-model="timed"
+              type="checkbox"
+              class="radio-button"
+              name="timed"
+              />
+              Timed
+            </label>
+          </div>
+          <div class="form-group">
+            <label class="checkbox" for="ratable">
+              <input
+                type="checkbox"
+                :id="id"
+                class="radio-button"
+                :value="value"
+                :name="name"
+              />
+              Ratable
+            </label>
+          </div> -->
+
+          <div class="form-group">
+            <button class="btn btn-primary btn-block" :disabled="loading">
+              <span
+                v-show="loading"
+                class="spinner-border spinner-border-sm"
+              ></span>
+              <span>Add Habit</span>
+            </button>
+          </div>
+        </div>
+      </form>
+
+      <div v-if="message" class="alert alert-danger">
+        {{ message }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Api from "../api";
+
+export default {
+  name: "HabitScheduler",
+  data() {
+    return {
+      timed: "",
+      ratable: "",
+      title: "",
+      category_name: "",
+      description: "",
+      loading: false,
+      message: "",
+    };
+  },
+  methods: {
+    handleAdd() {
+      this.loading = true;
+      this.message = "";
+      Api.scheduleHabit({ timed: this.timed, ratable: this.ratable, title: this.title, category_name: this.category_name, description: this.description  })
+        .then(() => {
+          this.loading = false;
+          this.$router.push("/admin/");
+        })
+        .catch((error) => {
+          console.log(error);
+          if (error.response) {
+            this.message = error.response.data.message;
+          }
+          this.loading = false;
+        });
+    },
+  },
+};
+</script>
---
+++ src/pages/ScheduleHabit.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Navbar location="createhabit"></Navbar>
+    <HabitScheduler></HabitScheduler>
+  </div>
+</template>
+
+<script>
+import Api from "../api";
+import { } from '../auth';
+import HabitScheduler from "../components/HabitScheduler.vue";
+import Habit from "../components/Habit.vue";
+import Navbar from "../components/Navbar.vue";
+import { getJwtToken } from "../auth";
+import { getUserIdFromToken } from "../auth";
+
+export default {
+  components: { HabitCreator, Habit, Navbar }
+};
+</script>
