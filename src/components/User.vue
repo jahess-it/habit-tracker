f72ae76ed284dfa76ea97e9b5c555207b1aec600
James Hess <jahess.it@gmail.com>
2022-03-19 16:22:39 -0600

    Added more confirm buttons

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div style="background-color: #ADD8E6; padding: 25px;">
     <p>Username: {{ user.username }}<span v-if="admin"> (admin)</span></p>
     <p>Email: {{ user.email }}</p>
     <p>Mobile Phone #: {{ user.mobile_phone }}</p>
@@ -32,8 +32,38 @@
           okVariant: "danger",
           okTitle: "Demote User"
         }).then((res) => {
-          //TODO: API call to demote user
-          console.log(`${this.user.username} demoted`)
+          if (res) {
+            //TODO: API call to demote user
+            console.log(`${this.user.username} demoted`);
+          }
+        }).catch((err) => {
+          console.log(err.response);
+        });
+      },
+      confirmPromote() {
+        this.$bvModal.msgBoxConfirm(`Promote basic user ${this.user.username} to admin user?`, {
+          title: "Promote User",
+          okVariant: "danger",
+          okTitle: "Promote to Admin"
+        }).then((res) => {
+          if (res) {
+            //TODO: API call to promote user
+            console.log(`${this.user.username} promoted`);
+          }
+        }).catch((err) => {
+          console.log(err.response);
+        });
+      },
+      confirmDelete() {
+        this.$bvModal.msgBoxConfirm(`Delete user ${this.user.username}?`, {
+          title: "Delete User",
+          okVariant: "danger",
+          okTitle: "Delete"
+        }).then((res) => {
+          if (res) {
+            //TODO: API call to delete user
+            console.log(`${this.user.username} deleted`);
+          }
         }).catch((err) => {
           console.log(err.response);
         });
