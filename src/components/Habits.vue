8510cd517f8767c45b5e5f85cdcbc816d507ba39
Kaden Hardy <me@kadenhardy.com>
2022-03-19 13:58:10 -0600

    getting delete habit to work

--- src/components/Habits.vue
+++ src/components/Habits.vue
@@ -26,7 +26,7 @@
     </form>
     <b-button
       variant="outline-danger"
-      @click="() => deleteHabit(habit.habit_id)"
+      @click="() => handleDelete(habit.habit_id)"
       >Delete</b-button
       >
     </p>
