2e3c978417e7216035de1cc7debef49323808ae3
Rhemsley <77403987+Rhemsley@users.noreply.github.com>
2022-03-19 02:16:42 -0600

    move button again

--- src/pages/Journals.vue
+++ src/pages/Journals.vue
@@ -5,12 +5,12 @@
     <div>
       <h1 style="text-align: center">Habit Tracker</h1>
     </div>
-    <button @click="goToCreatejournal()" class="btn btn-primary" style="float:right">
+    <div>
+      <h2 style="text-align: center">Your Journal</h2>
+    </div>
+    <button @click="goToCreatejournal()" class="btn btn-primary btn-block">
        <span>Log Journal Entry</span>
     </button>
-    <span>
-      <h2 style="text-align: center">Your Journal</h2>
-    </span>
     <div v-if="loading">Loading journals . . .</div>
     <div v-else class="card card-container p-4">
       <div v-for="journal in journals" :key="String(journal.day)">
