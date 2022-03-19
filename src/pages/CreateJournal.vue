fc6299dc15ad602ec540a11ab77200241640c2eb
Rhemsley <77403987+Rhemsley@users.noreply.github.com>
2022-03-19 02:34:55 -0600

    Temporarily add habit

--- src/pages/CreateJournal.vue
+++ src/pages/CreateJournal.vue
@@ -10,9 +10,10 @@ import Api from "../api";
 import { } from '../auth';
 import JournalCreator from "../components/JournalCreator.vue";
 import Navbar from "../components/Navbar.vue";
+import Habit from "../components/Habit.vue";
 import { getJwtToken } from "../auth";
 import { getUserIdFromToken } from "../auth";
 export default {
-  components: { JournalCreator, Navbar }
+  components: { JournalCreator, Habit, Navbar }
 };
 </script>
