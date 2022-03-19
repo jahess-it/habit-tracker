f10259a1a47dbb6c9de1b57ff660e9d3e429b9b8
Rhemsley <77403987+Rhemsley@users.noreply.github.com>
2022-03-19 04:28:37 -0600

    Add update competed function

--- src/components/Habit.vue
+++ src/components/Habit.vue
@@ -3,7 +3,7 @@
     <p><b>{{this.habit.title}}</b>
     Time Spent: {{this.habit.time_spent}} <button type="button">Input Time</button>
     Habit Rating: {{this.habit.habit_rating}} <button type="button">Input Rating</button>
-    Day: {{this.habit.day}}
+    Day: {{this.habit.day}} <button type="button" @click="goToRegister()"> Completed </button>
     </p>
   </div>
 </template>
@@ -17,6 +17,60 @@
     data: function () {
       return {};
     },
-    created: function () {}
+    created: function () {},
+    methods: {
+      updateCompleted() {
+        this.habit.complete = !this.habit.complete
+        Api.updateHabitInstance({ this.habit }).then((_) => {
+        }).catch((error) => {
+          console.log(error);
+          if (error.response) {
+            this.message = error.response.data.message;
+          }
+          this.loading = false;
+        });
+    }
   };
+</script>
+
+
+  data() {
+    return {
+      category_name: "",
+      loading: false,
+      message: "",
+      display_color: null,
+        options: [
+          { value: null, text: 'Please select a color' },
+          { value: '#C8E0FA', text: 'Blue' },
+          { value: '#F4ACA8', text: 'Red' },
+          { value: '#FFC8A1', text: 'Orange' },
+          { value: '#C3FAC2', text: 'Green' },
+          { value: '#FFEDB9', text: 'Yellow' },
+          { value: '#D3D3D3', text: 'Gray' }
+        ]
+    };
+  },
+  methods: {
+    handleAdd() {
+      this.loading = true;
+      this.message = "";
+      Api.addCategory({
+        category_name: this.category_name,
+        display_color: this.display_color,
+      })
+        .then(() => {
+          this.loading = false;
+          this.$router.push("/createhabit");
+        })
+        .catch((error) => {
+          console.log(error);
+          if (error.response) {
+            this.message = error.response.data.message;
+          }
+          this.loading = false;
+        });
+    },
+  },
+};
 </script>
