cf6e79bf02dbb23f261af8cc36167f7d03211460
Rhemsley <77403987+Rhemsley@users.noreply.github.com>
2022-03-19 03:46:51 -0600

    remove habit import

--- src/pages/CreateJournal.vue
+++ src/pages/CreateJournal.vue
@@ -10,10 +10,9 @@ import Api from "../api";
 import { } from '../auth';
 import JournalCreator from "../components/JournalCreator.vue";
 import Navbar from "../components/Navbar.vue";
-import Habit from "../components/Habit.vue";
 import { getJwtToken } from "../auth";
 import { getUserIdFromToken } from "../auth";
 export default {
-  components: { JournalCreator, Habit, Navbar }
+  components: { JournalCreator, Navbar }
 };
 </script>
