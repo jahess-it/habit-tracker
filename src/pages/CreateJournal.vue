10506832495dc36497b0e33337796d95ac72f5a4
Rhemsley <77403987+Rhemsley@users.noreply.github.com>
2022-03-19 02:28:05 -0600

    Make a CreateJournal page

---
+++ src/pages/CreateJournal.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Navbar location="createhabit"></Navbar>
+    <HabitCreator></HabitCreator>
+  </div>
+</template>
+
+<script>
+import Api from "../api";
+import { } from '../auth';
+import JournalCreator from "../components/JournalCreator.vue";
+import Navbar from "../components/Navbar.vue";
+import { getJwtToken } from "../auth";
+import { getUserIdFromToken } from "../auth";
+export default {
+  components: { JournalCreator, Navbar }
+};
+</script>
